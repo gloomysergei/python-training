b555cb6c109a3c9501dba99cdec4f96dbed4d85f
gloomysergei <almazopal@gmail.com>
2025-09-20 10:55:35 +0300

    feat(2-task): complete solution of the problem

--- hexlet_python_package/2-task/compare_version.py
+++ hexlet_python_package/2-task/compare_version.py
@@ -1,2 +1,16 @@
-def compare_version(lst):
-  return 'start'
+def compare_version(ver1, ver2):
+  [major1, minor1] = ver1.split('.') # [major1, minor1]
+  [major2, minor2] = ver2.split('.') # [major2, minor2]
+  if int(major1) > int(major2):
+    return 1
+  elif int(major1) < int(major2):
+    return -1
+  elif int(major1) == int(major2):
+    if int(minor1) > int(minor2):
+      return 1
+    elif int(minor1) < int(minor2):
+      return -1
+    else:
+      return 0
+
+print(compare_version('1.1', '1.1'))
